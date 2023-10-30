093aa893f8ba3f8c6cdd341f030fa91b1f9a7d32
draconclark <kelvinlagoa3@gmail.com>
2023-10-30 15:36:27 -0300

    commit

--- src/components/PostComments/newFile.tsx
+++ src/components/PostComments/newFile.tsx
@@ -1,32 +1,31 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 import App from '../../App';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
 
-    test('Deve adicionar novo comentário', ()=> {
-        render(<App/>)
+    test('Deve adicionar novo comentário', () => {
+        render(<App />);
         fireEvent.change(screen.getByTestId('text-coment'), {
             target: {
                 value: 'Bélissimo'
             }
-        })
-        fireEvent.change(screen.getByTestId('btn-adicionar'))
-        expect(screen.getByText('Bélissimo')).toBeInTheDocument()
-    })
-    test('Deve ter 2 comentários', ()=> {
-        render(<App/>)
+        });
+        fireEvent.change(screen.getByTestId('btn-adicionar'));
+        expect(screen.getByText('Bélissimo')).toBeInTheDocument();
+    });
+    test('Deve ter 2 comentários', () => {
+        render(<App />);
         fireEvent.change(screen.getByTestId('text-coment'), {
             target: {
                 value: 'Carona?'
             }
-        })
-        fireEvent.change(screen.getByTestId('btn-adicionar'))
-        expect(screen.getByText('Carona?')).toBeInTheDocument()
-    })
+        });
+        fireEvent.change(screen.getByTestId('btn-adicionar'));
+        expect(screen.getByText('Carona?')).toBeInTheDocument();
+    });
 });
